93ebe4b195cb0d73f1b4d6ffb8a045f0d19c28a6
Murat Çelik <muratcelik11@gmail.com>
2021-08-04 02:33:07 +0300

    via

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { CourseComponent } from './Components/course/course.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    CourseComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
